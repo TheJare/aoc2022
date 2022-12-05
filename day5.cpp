02f9eb3e8eae41c66d2e222b583c2444ba72987b
Javier Arevalo <jare@iguanademos.com>
2022-12-05 19:34:27 +0100

    A bit more stylish structure

--- day5.cpp
+++ day5.cpp
@@ -8,42 +8,33 @@ static void part(span<string> args, int part)
     ifstream f(fn);
     vector<string> stacks;
 
-    bool parsing_crates = true;
-
-    for (string line; getline(f, line);)
+    for (string line; getline(f, line) && !is_emptyorws(line);)
     {
-        if (is_emptyorws(line))
+        for (size_t i = 1, j = 0; i < line.length(); i += 4, ++j)
         {
-            parsing_crates = false;
-        }
-        else if (parsing_crates)
-        {
-            for (size_t i = 1, j = 0; i < line.length(); i += 4, ++j)
+            if (stacks.size() <= j)
             {
-                if (stacks.size() <= j)
-                {
-                    stacks.push_back(string());
-                }
-                if (isalpha(line[i]))
-                {
-                    stacks[j].push_back(line[i]);
-                }
+                stacks.push_back(string());
+            }
+            if (isalpha(line[i]))
+            {
+                stacks[j].push_back(line[i]);
             }
         }
-        else
+    }
+    for (string line; getline(f, line) && !is_emptyorws(line);)
+    {
+        size_t num, from, to;
+        string dummy;
+        istringstream il(line);
+        il >> dummy >> num >> dummy >> from >> dummy >> to;
+        string group = stacks[from - 1].substr(0, num);
+        stacks[from - 1].erase(0, num);
+        if (part == 1)
         {
-            size_t num, from, to;
-            string dummy;
-            istringstream il(line);
-            il >> dummy >> num >> dummy >> from >> dummy >> to;
-            string group = stacks[from - 1].substr(0, num);
-            stacks[from - 1].erase(0, num);
-            if (part == 1)
-            {
-                rs::reverse(group);
-            }
-            stacks[to - 1].insert(0, group);
+            rs::reverse(group);
         }
+        stacks[to - 1].insert(0, group);
     }
     cout << "round " << part << " result is ";
     rs::for_each(stacks, [](auto &stack)
